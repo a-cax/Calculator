306ea67b46cc6de813c8d49413da6ed7ebaf5a08
Piotr Andrzejewski <acax.piotrandrzejewski@gmail.com>
2022-02-11 18:17:18 +0100

    added generating JSON log file

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -74,6 +74,9 @@ namespace CalculatorProgram
 
                 Console.WriteLine("\n"); // Friendly linespacing.
             }
+
+            // Add call to close the JSON writer before return
+            calculator.Finish();
             return;
         }
     }
